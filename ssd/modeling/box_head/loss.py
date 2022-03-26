04ccf0d5404baf632fbb5157c9b76bf78fe7e1bc
dmzlingyin <dmzlingyin@163.com>
2022-03-26 19:33:36 +0800

    use focal loss

--- ssd/modeling/box_head/loss.py
+++ ssd/modeling/box_head/loss.py
@@ -1,6 +1,7 @@
 import torch.nn as nn
 import torch.nn.functional as F
 import torch
+from torch.autograd import Variable
 
 from ssd.utils import box_utils
 
@@ -13,7 +14,10 @@ class MultiBoxLoss(nn.Module):
          and Smooth L1 regression loss.
         """
         super(MultiBoxLoss, self).__init__()
+
+        # 更改损失函数为focal_loss
         self.neg_pos_ratio = neg_pos_ratio
+        self.focal_loss = FocalLoss(2,0.75)
 
     def forward(self, confidence, predicted_locations, labels, gt_locations):
         """Compute classification loss and smooth l1 loss.
@@ -31,7 +35,9 @@ class MultiBoxLoss(nn.Module):
             mask = box_utils.hard_negative_mining(loss, labels, self.neg_pos_ratio)
 
         confidence = confidence[mask, :]
-        classification_loss = F.cross_entropy(confidence.view(-1, num_classes), labels[mask], reduction='sum')
+        # classification_loss = F.cross_entropy(confidence.view(-1, num_classes), labels[mask], reduction='sum')
+        # focal_loss替换原来的函数
+        classification_loss = self.focal_loss(confidence.view(-1, num_classes), labels[mask])
 
         pos_mask = labels > 0
         predicted_locations = predicted_locations[pos_mask, :].view(-1, 4)
@@ -39,3 +45,35 @@ class MultiBoxLoss(nn.Module):
         smooth_l1_loss = F.smooth_l1_loss(predicted_locations, gt_locations, reduction='sum')
         num_pos = gt_locations.size(0)
         return smooth_l1_loss / num_pos, classification_loss / num_pos
+
+
+class FocalLoss(nn.Module):
+    def __init__(self, gamma=0, alpha=None, size_average=True):
+        super(FocalLoss, self).__init__()
+        self.gamma = gamma
+        self.alpha = alpha
+        if isinstance(alpha,(float,int)): self.alpha = torch.Tensor([alpha,1-alpha])
+        if isinstance(alpha,list): self.alpha = torch.Tensor(alpha)
+        self.size_average = size_average
+
+def forward(self, input, target):
+    if input.dim()>2:
+        input = input.view(input.size(0),input.size(1),-1)  # N,C,H,W => N,C,H*W
+        input = input.transpose(1,2)    # N,C,H*W => N,H*W,C
+        input = input.contiguous().view(-1,input.size(2))   # N,H*W,C => N*H*W,C
+    target = target.view(-1,1)
+
+    logpt = F.log_softmax(input)
+    logpt = logpt.gather(1,target)
+    logpt = logpt.view(-1)
+    pt = Variable(logpt.data.exp())
+
+    if self.alpha is not None:
+        if self.alpha.type()!=input.data.type():
+            self.alpha = self.alpha.type_as(input.data)
+        at = self.alpha.gather(0,target.data.view(-1))
+        logpt = logpt * Variable(at)
+
+    loss = -1 * (1-pt)**self.gamma * logpt
+    if self.size_average: return loss.mean()
+    else: return loss.sum()
